6f47f07a94314765855e7cc9e3ce36031fde4001
araketu <diogo.ghizoni@gmail.com>
2020-04-14 11:25:02 -0300

    commit

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,4 +1,4 @@
-
+from django.urls import path, include
 from django.contrib import admin
 from django.urls import path
 from .views import hello_world
@@ -6,4 +6,5 @@ from .views import hello_world
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/',hello_world),
+    path('blog/', include("website.urls"))
 ]
