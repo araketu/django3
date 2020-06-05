384eb7c0e939313dba6ee61e5a7e28a9fde50de7
araketu <diogo.ghizoni@outlook.com>
2020-06-05 09:02:42 -0300

    Update Templates

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def hello_blog(request):
+    data = {'name':' Curso Django3'}
+    # return HttpResponse('Blog')
+    return render (request, 'index.html',data)
